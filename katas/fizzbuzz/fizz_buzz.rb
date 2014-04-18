9e424ac5a529619c8bbfc0a88e84343378b4ba95
“dimegliof” <dimegliofernando@gmail.com>
2014-04-18 17:16:25 -0300

    fizzbuzz kata agregada

---
+++ katas/fizzbuzz/fizz_buzz.rb
@@ -0,0 +1,13 @@
+class FizzBuzz
+
+	#retorna Buzz en caso de que el parametro 
+	#sea multpilo de 5, Fizz caso contrario
+	def say(nro)
+		if (nro%5) == 0
+			"Buzz"
+		else
+		  "Fizz"
+	  end
+  end
+
+end
